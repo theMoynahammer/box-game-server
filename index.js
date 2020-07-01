01dbaa09933703f2ea2f043fb8427f2f88eb13d1
Connor Moynahan <connor.moynahan@ge.com>
2020-07-01 15:31:32 -0400

    updating

--- index.js
+++ index.js
@@ -11,10 +11,18 @@ app.use(async ctx => {
   await send(ctx, './index.html');
 });
 
+let currentState = null;
+
 io.on('connection', (socket) => {
-  // console.log('a user connected');
+  console.log('a user connected');
+  console.log(`current client count: ${io.engine.clientsCount}`)
+  socket.on('joining game', ()=>{
+    // console.log('right here', currentState)
+    currentState && io.emit('newState', currentState)
+  })
   socket.on('newState', (newState) => {
     console.log('newState: ' + JSON.stringify(newState));
+    currentState = {...newState};
     io.emit('newState', newState);
     // socket.on('chat message', (msg) => {
     //   console.log('message: ' + msg);
@@ -22,6 +30,7 @@ io.on('connection', (socket) => {
   });
   socket.on('disconnect', () => {
     console.log('user disconnected');
+    console.log(`current client count: ${io.engine.clientsCount}`)
   });
 });
 
